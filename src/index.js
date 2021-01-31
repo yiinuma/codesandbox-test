f1999524aec39935a1566b1b234592275ec912c0
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-01-31 21:25:55 +0900

    no18まで終了

---
+++ src/index.js
@@ -0,0 +1,38 @@
+/*
+ *const,let等の変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// var val1 = "再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+// val2 = "let変数を上書き";
+// console.log(val2);
+// let val2 = "再宣言";
+
+// const変数は上書き不可
+// const val3 = "const変数";
+// console.log(val3);
+// // val3 = "const変数を上書き";
+// const val3 = "再宣言";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+// val4.name = "jak";
+// val4.adress = "Hirosima";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat", "human"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5.slice(-1));
