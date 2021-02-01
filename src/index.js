c31d4fd117b253fb17ba42eff576b319b32ba98e
yiinuma <65111001+yiinuma@users.noreply.github.com>
2021-02-01 22:06:41 +0900

    NO25まで終了

--- src/index.js
+++ src/index.js
@@ -146,3 +146,19 @@
 //   }
 // });
 // console.log(newNameArr);
+/**
+ * 三項演算子
+ */
+// ある条件？　条件がtrueのとき　：　条件がfalesの時
+// const val1 = 1 >
+// const num = "1200";
+// console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています！" : "許容範囲です";
+// };
+// console.log(checkSum(50, 60));
